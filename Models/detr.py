122c00c39db2320e88a2914019841dca0fb63333
Jii111 <90593855+Jii111@users.noreply.github.com>
2023-11-30 10:14:31 +0900

    Create detr.py

---
+++ Models/detr.py
@@ -0,0 +1,173 @@
+# based on https://github.com/woctezuma/finetune-detr/blob/master/finetune_detr.ipynb
+
+!rm -r transformers
+!git clone -b upload_detr_no_timm https://github.com/nielsrogge/transformers.git
+!cd /content/transformers
+!pip install -q ./transformers
+!pip install -q pytorch-lightning
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+import torchvision
+import os
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, processor, train=True):
+        ann_file = os.path.join(img_folder, "_annotations.coco.json" if train else "_annotations.coco.json")
+        super(CocoDetection, self).__init__(img_folder, ann_file)
+        self.processor = processor
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        # feel free to add data augmentation here before passing them to the next step
+        img, target = super(CocoDetection, self).__getitem__(idx)
+
+        # preprocess image and target (converting target to DETR format, resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {'image_id': image_id, 'annotations': target}
+        encoding = self.processor(images=img, annotations=target, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze() # remove batch dimension
+        target = encoding["labels"][0] # remove batch dimension
+
+        return pixel_values, target
+
+from transformers import DetrImageProcessor
+
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+
+train_dataset = CocoDetection(img_folder='/content/drive/MyDrive/1129train.v2i.coco/aug_train', processor=processor)
+val_dataset = CocoDetection(img_folder='/content/drive/MyDrive/1129validd.v1i.coco/train', processor=processor, train=False)
+
+## 이미지 확인
+import numpy as np
+image_ids = train_dataset.coco.getImgIds()
+image_id = image_ids[np.random.randint(0, len(image_ids))] # 랜덤 이미지 선택
+print('Image n°{}'.format(image_id))
+image = train_dataset.coco.loadImgs(image_id)[0]
+image
+
+import numpy as np
+import os
+from PIL import Image, ImageDraw
+
+image_ids = train_dataset.coco.getImgIds()
+# let's pick a random image
+image_id = image_ids[np.random.randint(0, len(image_ids))]
+print('Image n°{}'.format(image_id))
+image = train_dataset.coco.loadImgs(image_id)[0]
+image = Image.open(os.path.join('/content/drive/MyDrive/1129train.v2i.coco/train', image['file_name']))
+
+annotations = train_dataset.coco.imgToAnns[image_id]
+draw = ImageDraw.Draw(image, "RGBA")
+
+cats = train_dataset.coco.cats
+id2label = {k: v['name'] for k,v in cats.items()}
+
+for annotation in annotations:
+  box = annotation['bbox']
+  class_idx = annotation['category_id']
+  x,y,w,h = tuple(box)
+  draw.rectangle((x,y,x+w,y+h), outline='red', width=1)
+  draw.text((x, y), id2label[class_idx], fill='white')
+
+image
+
+## 데이터 로드
+from torch.utils.data import DataLoader
+
+def collate_fn(batch):
+  pixel_values = [item[0] for item in batch]
+  encoding = processor.pad(pixel_values, return_tensors="pt")
+  labels = [item[1] for item in batch]
+  batch = {}
+  batch['pixel_values'] = encoding['pixel_values']
+  batch['pixel_mask'] = encoding['pixel_mask']
+  batch['labels'] = labels
+  return batch
+
+train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, batch_size=4, shuffle=True)
+val_dataloader = DataLoader(val_dataset, collate_fn=collate_fn, batch_size=2)
+batch = next(iter(train_dataloader))
+
+import pytorch_lightning as pl
+from transformers import DetrConfig, DetrForObjectDetection
+import torch
+
+class Detr(pl.LightningModule):
+     def __init__(self, lr, lr_backbone, weight_decay):
+         super().__init__()
+         # replace COCO classification head with custom head
+         # we specify the "no_timm" variant here to not rely on the timm library
+         # for the convolutional backbone
+         self.model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50",
+                                                             revision="no_timm",
+                                                             num_labels=len(id2label),
+                                                             ignore_mismatched_sizes=True)
+         # see https://github.com/PyTorchLightning/pytorch-lightning/pull/1896
+         self.lr = lr
+         self.lr_backbone = lr_backbone
+         self.weight_decay = weight_decay
+
+     def forward(self, pixel_values, pixel_mask):
+       outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+       return outputs
+
+     def common_step(self, batch, batch_idx):
+       pixel_values = batch["pixel_values"]
+       pixel_mask = batch["pixel_mask"]
+       labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
+
+       outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask, labels=labels)
+
+       loss = outputs.loss
+       loss_dict = outputs.loss_dict
+
+       return loss, loss_dict
+
+     def training_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)
+        # logs metrics for each training_step,
+        # and the average across the epoch
+        self.log("training_loss", loss)
+        for k,v in loss_dict.items():
+          self.log("train_" + k, v.item())
+
+        return loss
+
+     def validation_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)
+        self.log("validation_loss", loss)
+        for k,v in loss_dict.items():
+          self.log("validation_" + k, v.item())
+
+        return loss
+
+     def configure_optimizers(self):
+        param_dicts = [
+              {"params": [p for n, p in self.named_parameters() if "backbone" not in n and p.requires_grad]},
+              {
+                  "params": [p for n, p in self.named_parameters() if "backbone" in n and p.requires_grad],
+                  "lr": self.lr_backbone,
+              },
+        ]
+        optimizer = torch.optim.AdamW(param_dicts, lr=self.lr,
+                                  weight_decay=self.weight_decay)
+
+        return optimizer
+
+     def train_dataloader(self):
+        return train_dataloader
+
+     def val_dataloader(self):
+        return val_dataloader
+
+## 훈련
+model = Detr(lr=1e-4, lr_backbone=1e-5, weight_decay=1e-4)
+outputs = model(pixel_values=batch['pixel_values'], pixel_mask=batch['pixel_mask'])
+
+from pytorch_lightning import Trainer
+#trainer = Trainer(gpus=1, max_steps=300, gradient_clip_val=0.1)
+trainer = Trainer(max_steps=300, gradient_clip_val=0.1)
+trainer.fit(model)
